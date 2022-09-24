5f451f177abfe8ce94685b61c8f1c89a1ed9997e
Marieli <marielirodrigues888@gmail.com>
2022-09-24 11:12:52 -0300

    Calculadora e CNH

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,17 @@
-const { somar } = require("../src/calculator");
+const { somar, diminuir, multiplicar,dividir} = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
+});
+
+test("Diminuir dois valores válidos", async () => {
+  expect(diminuir(15, 5)).toStrictEqual(10);
+});
+
+test("Multiplicar dois valores válidos", async () => {
+  expect(multiplicar(3, 5)).toStrictEqual(15);
+});
+
+test("Dividir dois valores válidos", async () => {
+  expect(dividir(15, 3)).toStrictEqual(5);
 });
--- test/cnh.test.js
+++ test/cnh.test.js
@@ -1,5 +1,9 @@
 const { obterCnh } = require("../src/cnh");
 
-test("test", async () => {
-    
+test("Valida CNH idade igual 18", async () => {
+    expect(obterCnh(18)).toBeTruthy();
+});
+
+test("Valida CNH idade menor 18", async () => {
+    expect(obterCnh(15)).toBeFalsy();
 });
